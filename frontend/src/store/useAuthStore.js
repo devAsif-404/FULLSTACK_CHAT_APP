bae9c9b3249342691586f2976840c0a5380eff00
Asif Shaikh <asifshaikh202121@gmail.com>
2025-07-08 13:51:20 +0530

    login state updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -8,7 +8,7 @@ const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
-  isLoggingIn: false,
+  isLogginIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
